69e359739f70f5e7e0004f30060b64c21def8367
Avinash Patel <patelavinash508@gmail.com>
2023-02-28 20:23:22 +0530

    refactor

--- src/main/java/com/gmaillistener/listener/EventSubscriberService.java
+++ src/main/java/com/gmaillistener/listener/EventSubscriberService.java
@@ -50,6 +50,15 @@ public class EventSubscriberService {
 	@Autowired
 	private GenericRestCalls genericRestCalls;
 
+	/**
+	 * admin : The emailId using which the service account was made.
+	 * 
+	 * Templfile delete missing.
+	 * 
+	 * @param serviceAccFileUrl
+	 * @param admin
+	 * @throws Exception
+	 */
 	public void gMessageListener(String serviceAccFileUrl, String admin) throws Exception {
 
 		String filePath = getServiceAccountDestailsAsStream(serviceAccFileUrl);
@@ -57,7 +66,8 @@ public class EventSubscriberService {
 		GoogleCredential driveService = getGoogleCredential(filePath);
 
 		String tokenValue = GoogleCredentials.fromStream(new FileInputStream(filePath))
-				.createScoped(Arrays.asList(scopesForGmailApis)).refreshAccessToken().getTokenValue();
+				.createScoped(Arrays.asList(scopesForGmailApis)).createDelegated(admin).refreshAccessToken()
+				.getTokenValue();
 
 		log.info("----Token : {}", tokenValue);
 
